32129338da7d9b170292681db3c2ff338aa19df2
kamseea <138553231+kamseea@users.noreply.github.com>
2023-07-11 05:36:37 +0800

    Update login.php

--- calana/login.php
+++ calana/login.php
@@ -14,13 +14,28 @@
     <link rel="stylesheet" href="style.css">
 
     <style>
-        body,html{
+.box {
+  width: 500px; 
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.2); 
+  padding: 20px;
+  position: absolute;
+  top: 50%;
+  left: 30%;
+  transform: translate(-50%, -50%);
+  border-radius: 20px;
+  backdrop-filter: blur(10px);
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1); 
+}
+ body,html {
     margin: 0;
     padding: 0;
     width: 100%;
     height: 100vh;
     font-family: sans-serif;
-}
+    overflow-x: hidden; 
+  }
+  
 .Section_top{
     width: 100%;
     height: 100%;
@@ -35,10 +50,9 @@
     animation: change 10s infinite ease-in-out;
 }
 .content{
-    position: absolute;
+    text-align: center;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
     text-transform: uppercase;
 
 }
@@ -87,12 +101,71 @@
         background-image: url(img/pikachu.png);
     }
 }
+
+
+
+.container {
+  width: 400px;
+  margin: 0 auto;
+}
+
+.container1 {
+  margin: 0 auto;
+}
+
+
+h2 {
+  text-align: center;
+}
+
+input[type=text] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #e6e2d3;
+  border-radius: 10px;
+  box-sizing: border-box;
+  background-color: rgba(255, 255, 255, 0.5); 
+}
+
+input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #e6e2d3;
+  border-radius: 10px;
+  box-sizing: border-box;
+  background-color: rgba(255, 255, 255, 0.5); 
+  
+}
+
+
+input[type=submit] {
+  width: 200px;
+  background-color: #5072A7;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: rgba(73, 98, 158, 0.5); 
+  
+}
+
+input[type=submit]:hover {
+  background-color:#5F9EA0 ;
+}
+
         </style>
 
 </head>
 <body>
 
 <div class="Section_top">
+    <div class="box">
         <div class="content">
             <h1>LOGIN</h1>
         </div>
@@ -102,7 +175,7 @@
             Email <br> 
             <input type="text" name="email" class="form-control"  placeholder="sample@gmail.com"><br>
             Password <br> 
-            <input type="password" name="password" class="form-control"><br>
+            <input type="password" name="password" class="form-control"><br></div>
             <div>
             <?php
                         if (isset($_SESSION['notfound_email'])) :
