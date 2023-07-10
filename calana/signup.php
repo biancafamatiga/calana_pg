56e231def1f437d5750cd3b58fd25dff06ab0475
Haelena Tabara <131349740+haelena@users.noreply.github.com>
2023-07-11 05:36:39 +0800

    Update signup.php

--- calana/signup.php
+++ calana/signup.php
@@ -13,13 +13,75 @@
     <link rel="stylesheet" href="style.css">
 
     <style>
-        body,html{
+
+    body,html{
     margin: 0;
     padding: 0;
     width: 100%;
     height: 100vh;
     font-family: sans-serif;
 }
+
+.sign-up-container {
+    backdrop-filter: blur(10px);
+	background-color: rgba(255, 255, 255, 0.2); 
+	border-radius: 20px;
+	padding: 10px;
+	width: 700px;
+	margin: 50px auto;
+    position:relative; right:300px; top:80px;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1); 
+}
+input[type=text]{
+    width: 20%;
+     padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #e6e2d3;
+  border-radius: 10px;
+  box-sizing: border-box;
+
+}
+input[type=text], [type=password] {
+  width: 50%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #e6e2d3;
+  border-radius: 10px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 200px;
+  background-color: #5072A7;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+input[type=button] {
+  background-color: #5072A7;
+  color: white;
+  padding: 7px 10px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  opacity:0.7;
+}
+
+input[type=submit]:hover {
+  background-color:#5F9EA0 ;
+}
+
+input[type=button]:hover {
+  background-color:#5F9EA0 ;
+}
+
 .Section_top{
     width: 100%;
     height: 100%;
@@ -41,10 +103,16 @@
     text-transform: uppercase;
 
 }
+h1{
+    position: relative;
+    right:290px;
+    bottom:300px;
+}
 .content h1{
     color: #fff;
     font-size: 60px;
     letter-spacing: 15px;
+    
 }
 .content h1 span{
     color: #111;
@@ -60,6 +128,8 @@
     background: #034e88;
     color: #fff;
 }
+
+
 @keyframes change{
     0%
     {
@@ -95,68 +165,71 @@
         <div class="content">
             <h1>SIGN UP</h1>
         </div>
-        
-        <div class="container">
-            <form action="validate_signup.php" method="post" autocomplete="on">
-            Player ID <br> 
-            <div class ="pid">
-            <input name="pid1" id="pid1" type="text" class="form-control" placeholder="XXXX" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" minlength="4" maxlength="4" required>
-            -
-            <input name="pid2" id="pid2" type="text" class="form-control" placeholder="XXXX" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" minlength="4" maxlength="4"required>
-            -
-            <input name="pid3" id="pid4" type="text" class="form-control" placeholder="XXXX"  oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" minlength="4" maxlength="4"required>
-            </div>
-            Email <br> 
-            <input type="text" name="email" class="form-control"  placeholder="sample@gmail.com"><br>
-            Password <br> 
-            <input type="password" name="password" class="form-control"><br>
-            Confirm Password <br> 
-            <input type="password" name="conpass" class="form-control"><br>
+            <div class = "sign-up-container">
+                <div class="container">
+                    <form action="validate_signup.php" method="post" autocomplete="on">
+                    Player ID <br> 
+                        <div class ="pid">
+                        <input name="pid1" id="pid1" type="text" class="form-control" placeholder="XXXX" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" minlength="4" maxlength="4" required>
+                        -
+                        <input name="pid2" id="pid2" type="text" class="form-control" placeholder="XXXX" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" minlength="4" maxlength="4"required>
+                        -
+                        <input name="pid3" id="pid4" type="text" class="form-control" placeholder="XXXX"  oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" minlength="4" maxlength="4"required>
+                        </div>
+                    Email <br> 
+                    <input type="text" name="email" class="form-control"  placeholder="sample@gmail.com"><br>
+                    Password <br> 
+                    <input type="password" name="password" class="form-control"><br>
+                    Confirm Password <br> 
+                    <input type="password" name="conpass" class="form-control"><br>
 
-            <?php
-                        if (isset($_SESSION['existing_email'])) :
-                    ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php
-                            if (isset($_SESSION['existing_email'])) :
-                                echo $_SESSION['existing_email'];
-                                unset($_SESSION['existing_email']);
-                            endif;
-                        ?>
-                    </div>
                     <?php
-                        endif;
-                        if (isset($_SESSION['error_email'])) :
-                    ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php
-                            if (isset($_SESSION['error_email'])) :
-                                echo $_SESSION['error_email'];
-                                unset($_SESSION['error_email']);
-                            endif;
-                        ?>
-                    </div>
-                    <?php
-                        endif;
-                        if (isset($_SESSION['error_conpass'])) :
-                    ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php
-                            if (isset($_SESSION['error_conpass'])) :
-                                echo $_SESSION['error_conpass'];
-                                unset($_SESSION['error_conpass']);
-                            endif;
-                        ?>
-                    </div>
-                    <?php
-                        endif;
-                    ?>
+                                if (isset($_SESSION['existing_email'])) :
+                            ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?php
+                                    if (isset($_SESSION['existing_email'])) :
+                                        echo $_SESSION['existing_email'];
+                                        unset($_SESSION['existing_email']);
+                                    endif;
+                                ?>
+                            </div>
+                            <?php
+                                endif;
+                                if (isset($_SESSION['error_email'])) :
+                            ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?php
+                                    if (isset($_SESSION['error_email'])) :
+                                        echo $_SESSION['error_email'];
+                                        unset($_SESSION['error_email']);
+                                    endif;
+                                ?>
+                            </div>
+                            <?php
+                                endif;
+                                if (isset($_SESSION['error_conpass'])) :
+                            ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?php
+                                    if (isset($_SESSION['error_conpass'])) :
+                                        echo $_SESSION['error_conpass'];
+                                        unset($_SESSION['error_conpass']);
+                                    endif;
+                                ?>
+                            </div>
+                            <?php
+                                endif;
+                            ?>
 
-            <input type="submit"  class="btn btn-primary" value="Sign Up"><br>
+                    <input type="submit"  class="btn btn-primary" value="Sign Up"><br>
 
-            Already Registered?
-            <input type="button" id="bt" class="btn btn-primary" value="Login" onclick="location='login.php' " />
-    </div> 
+                    Already Registered?
+                    <input type="button" id="bt" class="btn btn-primary" value="Login" onclick="location='login.php' " />
+                </div>
+   
+    
+            </div> 
 </div>
     
 </body>
