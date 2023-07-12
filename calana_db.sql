a555a48acf6773210c878d78889002fef941ace6
biancafamatiga <91246724+biancafamatiga@users.noreply.github.com>
2023-07-12 22:53:04 +0800

    Add files via upload

--- calana_db.sql
+++ calana_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2023 at 07:07 PM
+-- Generation Time: Jul 12, 2023 at 04:47 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -54,6 +54,45 @@ CREATE TABLE `items` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `item_name`, `item_price`) VALUES
+(1, '1x Incubator Limited', 150),
+(2, '1x Super Incubator', 200),
+(3, '1x Premium Battle Pass', 100),
+(4, '3x Premium Battle Pass', 250),
+(5, '1x Remote Raid Pass', 195),
+(6, '3x Remote Raid Pass', 525),
+(7, '1x Rocket Radar', 200),
+(8, '1x Poffin', 100),
+(9, '20x Poke Ball', 100),
+(10, '100x Poke Ball', 460),
+(11, '200x Poke Ball', 800),
+(12, '1x Incense', 40),
+(13, '8x Incense', 250),
+(14, '1x Star Piece', 100),
+(15, '8x Star Piece', 640),
+(16, '10x Max Potion', 200),
+(17, '6x Max Revive', 180),
+(18, '1x Lucky Egg', 80),
+(19, '8x Lucky Egg', 500),
+(20, '1x Glacial Lure Module', 180),
+(21, '1x Mossy Lure Module', 180),
+(22, '1x Magnetic Lure Module', 180),
+(23, '1x Rainy Lure Module', 180),
+(24, '1x Lure Module', 180),
+(25, '8x Lure Module', 680),
+(26, 'Bag Upgrade', 200),
+(27, 'Pokemon Storage Upgrade', 200),
+(28, 'Postcard Pages', 100),
+(29, '10x Togedemaru Sticker', 35),
+(30, '10x Wartortle Sticker', 35),
+(31, '10x Axew Sticker', 35),
+(32, '10x Butterfree Sticker', 35),
+(33, '10x Pikachu Sticker', 35);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -83,7 +122,7 @@ ALTER TABLE `calana_users`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
