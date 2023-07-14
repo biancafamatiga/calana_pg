669181ba34a8d3b32409c4c88f09e36bcefb0fc6
kamseea <138553231+kamseea@users.noreply.github.com>
2023-07-14 12:37:29 +0800

    Update store.php

--- calana/store.php
+++ calana/store.php
@@ -6,34 +6,163 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
+ 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
 
     <title>Pokemon Go Calana</title>
-<style>
+    <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700;800&display=swap');
     
 
     html {
-    font-size: 50.5%;
     font-family: 'Poppins', sans-serif;
     scroll-behavior: smooth;
     }
 
+
+    .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  .items-section {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+
+  .item-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 200px;
+    padding: 10px;
+    background-color: #f5f5f5;
+    border-radius: 15px;
+  }
+
+  .images-items {
+    width: 125px;
+    height: 125px;
+    object-fit: cover;
+    border-radius: 50%;
+    margin-bottom: 10px;
+  }
+
+  .card-inner {
+    margin-bottom: 10px;
+  }
+
+  .card-price {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .add-cart {
+    margin-top: 10px;
+  }
+
+  .checkout-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.checkout-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #ec994b;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.items-title {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 24px;
+  color: #333;
+}
+
+.item-container:hover {
+  transform: scale(1.1);
+  background-color: #eaf0ff; 
+}
+
+
+.images-items {
+  width: 125px;
+  height: 125px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-bottom: 10px;
+}
+
+.card-inner {
+  margin-bottom: 10px;
+}
+
+.card-price {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.add-cart {
+  margin-top: 10px;
+}
+
+.checkout-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.checkout-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #ec994b;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.items-title {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 24px;
+  color: #333;
+}
+
+.add-cart input[type="submit"] {
+  margin-top: 10px;
+  padding: 8px 16px;
+  font-size: 10px;
+  background-color: #5D76A9; 
+  color: #fff;
+  border: none;
+  border-radius: 20px;  
+  cursor: pointer;
+}
     
-    
+
 </style>
 
 </head>
 <body>
-
-<!--Start Items-->
-<h1> STORE ITEMS </h1>
 
 <?php 
     $store_items = array(
@@ -218,6 +347,7 @@
 ?>
 
 <div class="container flex-1 w-80">
+    <h1 class="items-title">STORE ITEMS</h1>
     <div class="items-section">
         <?php foreach ($store_items as $key => $item) { ?>
             <div class="item-container">
@@ -228,30 +358,31 @@
                         <img src="https://static.wikia.nocookie.net/pokemongo/images/8/89/PokeCoin.png" width="21" height="20"><?= $item["item_price"] ?>
                     </div>
                     <div class="add-cart">
-                    <form method="post">
-                        <input type="hidden" name="item_id" value="<?= $key ?>">
-                        <input type="submit" name="added_item" value="Add to Cart">
-                    </form>
-                     </div>
+                        <form method="post">
+                            <input type="hidden" name="item_id" value="<?= $key ?>">
+                            <input type="submit" name="added_item" value="Add to Cart">
+                        </form>
+                    </div>
                 </div>
             </div>
         <?php } ?>
-        <div class="payment">
+    </div>
+    <div class="checkout-container">
         <form method="post" action="payment.php">
-                    <input type="hidden" name="item_id" value="<?= $key ?>">
-                    <button type="submit" name="payment"> <a class="nav-link" href="payment.php"> Checkout </a></button>
-                </form>
-        </div>
+            <button type="submit" name="payment" class="checkout-button">Checkout</button>
+        </form>
     </div>
 </div>
+</div>
+
 
 <!--End Items-->
-
+<br><br>
 
 
 </body>
 </html>
 
-<?php
+  <?php
     require("include/footer.php");
-?>
+  ?>
