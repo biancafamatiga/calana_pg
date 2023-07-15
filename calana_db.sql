d9912eaa4c4ad5873ca583cdfd0ea7e953f7b3fa
kamseea <138553231+kamseea@users.noreply.github.com>
2023-07-15 11:59:25 +0800

    Add files via upload

--- calana_db.sql
+++ calana_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2023 at 07:01 PM
+-- Generation Time: Jul 10, 2023 at 07:07 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -39,10 +39,7 @@ CREATE TABLE `calana_users` (
 --
 
 INSERT INTO `calana_users` (`id`, `player_id`, `email`, `password`) VALUES
-(16, '1111-1111-1111', 'bfmtg313@gmail.com', 'slay'),
-(17, '1234-1234-1234', 'lisa@gmail.com', '1020'),
-(18, '0000-0000-0000', 'slayerage@gmail.com', 'qqqq'),
-(19, '2222-2222-2222', 'ysabelfam@gmail.com', 'aaaa');
+(16, '1111-1111-1111', 'bfmtg313@gmail.com', 'slay');
 
 -- --------------------------------------------------------
 
@@ -55,45 +52,6 @@ CREATE TABLE `items` (
   `item_name` varchar(50) NOT NULL,
   `item_price` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `items`
---
-
-INSERT INTO `items` (`item_id`, `item_name`, `item_price`) VALUES
-(1, '1x Incubator Limited', 150),
-(2, '1x Super Incubator', 200),
-(3, '1x Premium Battle Pass', 100),
-(4, '3x Premium Battle Pass', 250),
-(5, '1x Remote Raid Pass', 195),
-(6, '3x Remote Raid Pass', 525),
-(7, '1x Rocket Radar', 200),
-(8, '1x Poffin', 100),
-(9, '20x Poke Ball', 100),
-(10, '100x Poke Ball', 460),
-(11, '200x Poke Ball', 800),
-(12, '1x Incense', 40),
-(13, '8x Incense', 250),
-(14, '1x Star Piece', 100),
-(15, '8x Star Piece', 640),
-(16, '10x Max Potion', 200),
-(17, '6x Max Revive', 180),
-(18, '1x Lucky Egg', 80),
-(19, '8x Lucky Egg', 500),
-(20, '1x Glacial Lure Module', 180),
-(21, '1x Mossy Lure Module', 180),
-(22, '1x Magnetic Lure Module', 180),
-(23, '1x Rainy Lure Module', 180),
-(24, '1x Lure Module', 180),
-(25, '8x Lure Module', 680),
-(26, 'Bag Upgrade', 200),
-(27, 'Pokemon Storage Upgrade', 200),
-(28, 'Postcard Pages', 100),
-(29, '10x Togedemaru Sticker', 35),
-(30, '10x Wartortle Sticker', 35),
-(31, '10x Axew Sticker', 35),
-(32, '10x Butterfree Sticker', 35),
-(33, '10x Pikachu Sticker', 35);
 
 --
 -- Indexes for dumped tables
@@ -119,13 +77,13 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `calana_users`
 --
 ALTER TABLE `calana_users`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `item_id` int(15) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
