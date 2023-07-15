a41666de008dac4af76307de6bbe6e454279296d
biancafamatiga <91246724+biancafamatiga@users.noreply.github.com>
2023-07-16 00:20:15 +0800

    Update conn.php

--- calana/conn.php
+++ calana/conn.php
@@ -1,9 +1,9 @@
 <?php
     // Declare variables for connection
     $database = 'calana_db';
-    $username = 'root';
-    $password = '';
-    $host = 'localhost';
+    $username = 'if0_34621220';
+    $password = 'slayerz123';
+    $host = 'ftpupload.net';
     
     // Create connection using object-oriented style
     $conn = new mysqli($host, $username, $password, $database);
